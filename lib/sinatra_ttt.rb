251d3a84d633004989c9512f96d671a3d6b0226d
Philip Szalwinski <pszalwinski@gmail.com>
2013-07-15 16:24:15 -0500

    Adding html pathways

--- lib/sinatra_ttt.rb
+++ lib/sinatra_ttt.rb
@@ -1,20 +1,37 @@
 require 'sinatra'
 require './lib/sinatra_ui'
+require 'ttt'
 
 class SinatraTTT < Sinatra::Base
   get '/' do
     erb :welcome
   end
-  
-  post '/config' do
-    params[:player]
-  end
-  
+
   get '/config' do
     erb :configure
+  end  
+  
+  post '/config' do
+    TTT.configure_game(params[:marker], params[:opponent], params[:board_size])
   end
+  
+  post '/make_move' do
+    TTT.make_move(params[:square], params[:marker])
+  end
+
+  get '/output_board' do
+    TTT.output_board(params[:board])
+  end  
 
   post '/game' do
      SinatraUI.print_board(' 1 | 2 | 3 ') 
+  end
+  
+  get '/restart' do
+    erb :welcome
+  end
+  
+  post '/restart' do
+    TTT.restart(params[:restart_choice])
   end
 end
--- spec/sinatra_ttt_spec.rb
+++ spec/sinatra_ttt_spec.rb
@@ -6,21 +6,57 @@ def app
 end
 
 describe SinatraTTT do
-  describe '/' do
+  describe 'ttt home page' do
     it 'retrieves a 200 response' do
       get '/'
       last_response.status.should == 200
     end
   end
   
-  describe '/game' do
-    it 'retrieves board and player options' do
-      post '/game', {"board_size" => '3', "player_one" => "h",
-                     "player_two" => "h"}
-      last_response.status.should == 200
-      last_response.body.should include(" 1 | 2 | 3 ")
+  context 'configuring the game' do
+    it 'sends the opponent type, board size and player peice to be configured' do
+      marker     = 'X'
+      opponent   = 'human'
+      board_size = '3'
+      TTT.should_receive(:configure_game).with(marker, opponent, board_size)
+      
+      post '/config', marker: marker, opponent: opponent, board_size: board_size
     end
   end
+  
+  context 'making a move on the board' do
+    it 'sends a selected move to the board' do
+      marker = 'X'
+      square   = '1'      
+      TTT.should_receive(:make_move).with(square, marker)
+      
+      post '/make_move', marker: marker, square: square
+    end
+  end
+  
+  context 'outputting the board' do
+    it 'puts the board on the screen in the browser' do
+      board = '123456789'
+      TTT.should_receive(:output_board).with(board)
+      
+      get '/output_board', board: '123456789'
+    end
+  end
+  
+  context 'restarting the game' do
+    it 'asks user to restart' do      
+      get '/restart'
+      last_response.status.should == 200
+    end
+    
+    it 'sends a selection to restart' do
+      restart_choice = 'yes'
+      TTT.should_receive(:restart).with(restart_choice)
+      
+      post '/restart', restart_choice: restart_choice
+    end
+  end
+  
   # Test that params contains options for player
   # Start game should be at root and that ^
 end
